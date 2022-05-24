8f36ab3e5058373b924256f5dcec393cab5eb810
Meenakshi Rana <dgrmeenakshi@gmail.com>
2022-05-24 15:05:05 +0530

    Initial Commit

---
+++ SampleProject/Base.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+namespace SampleProject
+{
+    [TestFixture]
+    public class Base
+    {
+        [Test]
+        public void LaunchApplication()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Meenakshi\SampleProject\packages\Selenium.WebDriver.ChromeDriver.101.0.4951.4100\driver\win32\");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Quit();
+
+        }
+    }
+}
---
+++ SampleProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32a432f5-87dc-4972-bf33-720bdb877d52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
